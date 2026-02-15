1923eb2d982c786de83e9616df12cd072b512f55
Shakhawat <srabonshakhawat@gmail.com>
2026-02-16 04:26:35 +0600

    first commit

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+import { USER_ROLES } from '../../../enums/user';
+
+export type IUser = {
+  name: string;
+  role: USER_ROLES;
+  contact: string;
+  email: string;
+  password: string;
+  image?: string;
+  avatar?: string;
+  status: 'active' | 'delete';
+  verified: boolean;
+  provider?: 'local' | 'google' | 'facebook' | 'github';
+  providerId?: string;
+  authentication?: {
+    isResetPassword: boolean;
+    oneTimeCode: number;
+    expireAt: Date;
+  };
+};
+
+export type UserModal = {
+  isExistUserById(id: string): any;
+  isExistUserByEmail(email: string): any;
+  isMatchPassword(password: string, hashPassword: string): boolean;
+} & Model<IUser>;
